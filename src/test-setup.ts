8374760ba81054390b393b631fcd0b3df91842a1
leolanese <laneseleo@gmail.com>
2026-01-04 18:32:06 +0000

    Updating Vitest setup

---
+++ src/test-setup.ts
@@ -0,0 +1,7 @@
+// This file is used to set up global configurations for Vitest
+
+// Example: Mocking global objects or setting up test environments
+import '@testing-library/jest-dom';
+
+// Add any global setup code here
+console.log('Global test setup loaded.');
